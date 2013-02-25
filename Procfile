17700a1217338b8bc768e3b5240218e806c6330e
Alex Byrnes <alex@nuams.co>
2013-02-25 09:05:18 -0600

    first commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: python open_data_api.py
+
---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+import urlparse
+
+urlparse.uses_netloc.append('postgres')
+url = urlparse.urlparse(os.environ.get('HEROKU_POSTGRESQL_PURPLE_URL'))
+
+
+DEBUG = True 
+SERVER_NAME = '0.0.0.0'
+SERVER_PORT = 80
+DB_CONNECTION = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
+METADATA_DB = 'metadb'
+METADATA_COLLECTION = 'metadata'
+
